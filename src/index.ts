ea4522498f790dd344e1f93107def4712d579324
Loup Peluso <loup.peluso@gmail.com>
2018-06-18 12:25:08 +0200

    First version

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,46 @@
-import * as utils from "./utils";
-import { Graph } from "./utils";
+import { ArgumentParser } from "argparse";
+import { readExportFile, readResultFile, printAllInducedFailures, createIndex, Graph } from "./utils";
 
-const exportFile = utils.readExportFile("C:\\dev\\alcuin\\atls\\talentevo.json");
-const resultFile = utils.readResultFile("C:\\dev\\alcuin\\atls\\result.json");
-const index = utils.createIndex(exportFile);
+const SHOW_INDUCED_ERRORS_TYPE = "show-induced-errors";
 
-utils.printAllInducedFailures(new Graph(index), resultFile);
+const argparser = new ArgumentParser({
+    version: '0.0.1',
+    addHelp: true,
+    description: "Tools for productivity"
+});
+
+const subparser = argparser.addSubparsers({
+    dest: "type"
+});
+
+const showInducedErrorParser = subparser.addParser(SHOW_INDUCED_ERRORS_TYPE);
+
+showInducedErrorParser.addArgument(["-s", "--source-file"], {
+    required: true,
+});
+
+showInducedErrorParser.addArgument(["-e", "--result-file"], {
+    required: true,
+});
+
+interface ShowInducedErrorsParser {
+    type: 
+        | typeof SHOW_INDUCED_ERRORS_TYPE;
+    source_file: string;
+    result_file: string;
+}
+
+type AnyParser =
+    | ShowInducedErrorsParser
+
+const args = argparser.parseArgs() as AnyParser;
+
+if (args.type === SHOW_INDUCED_ERRORS_TYPE) {
+    const source = readExportFile(args.source_file);
+    const results = readResultFile(args.result_file);
+
+    const index = createIndex(source);
+    const graph = new Graph(index);
+
+    printAllInducedFailures(graph, results);
+}
